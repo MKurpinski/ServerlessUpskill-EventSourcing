0ba5bf6563590ebd30616d9e0dcd2a5dd308f0d2
Michał Kurpiński <michal.kurpinski96@gmail.com>
2020-02-01 19:05:52 +0100

    Working flow of rebuilding read model of category

---
+++ Upskill/Category.Api/Functions/Category/Rebuild/CategoryEntity.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading.Tasks;
+using Category.Search.Dtos;
+using Category.Search.Indexers;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Newtonsoft.Json;
+using Upskill.EventStore;
+
+namespace Category.Api.Functions.Category.Rebuild
+{
+    public interface ICategoryEntity
+    {
+        Task Rebuild();
+        Task Delete();
+    }
+
+    public class CategoryEntity : ICategoryEntity
+    {
+        public Core.Aggregates.Category Category { get; private set; }
+
+        [JsonIgnore]
+        private readonly ISearchableCategoryIndexer _categoryIndexer;
+        [JsonIgnore]
+        private readonly IEventStore<Core.Aggregates.Category> _eventStore;
+
+        public CategoryEntity(
+            ISearchableCategoryIndexer categoryIndexer,
+            IEventStore<Core.Aggregates.Category> eventStore)
+        {
+            _categoryIndexer = categoryIndexer;
+            _eventStore = eventStore;
+        }
+
+        public async Task Rebuild()
+        {
+            var aggregateResult = await _eventStore.AggregateStream(Entity.Current.EntityKey);
+            if (!aggregateResult.Success)
+            {
+                return;
+            }
+
+            this.Category = aggregateResult.Value;
+            var toIndex = new CategoryDto(
+                this.Category.Id,
+                this.Category.Name, 
+                this.Category.Description,
+                this.Category.SortOrder);
+
+            await _categoryIndexer.Reindex(toIndex);
+        }
+
+        public Task Delete()
+        {
+            Entity.Current.DeleteState();
+            return Task.CompletedTask;
+        }
+
+        [FunctionName(nameof(CategoryEntity))]
+        public static Task Run([EntityTrigger] IDurableEntityContext ctx)
+            => ctx.DispatchAsync<CategoryEntity>();
+    }
+}
--- Upskill/Category.Api/Functions/Category/Rebuild/RebuildReadModelProcessOrchestrator.cs
+++ Upskill/Category.Api/Functions/Category/Rebuild/RebuildReadModelProcessOrchestrator.cs
@@ -1,19 +1,44 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 
 namespace Category.Api.Functions.Category.Rebuild
 {
-    public static class RebuildReadModelProcessOrchestrator
+    public class RebuildReadModelProcessOrchestrator
     {
         [FunctionName(nameof(RebuildReadModelProcessOrchestrator))]
-        public static async Task RunOrchestrator(
-            [OrchestrationTrigger] IDurableOrchestrationContext context,
-            [DurableClient] IDurableOrchestrationClient processStarter)
+        public async Task RunOrchestrator(
+            [OrchestrationTrigger] IDurableOrchestrationContext context)
         {
-            //await context.CallActivityAsync(nameof(StartReindex), null);
+            await context.CallActivityAsync(nameof(StartReindex), null);
 
-            //await context.CallActivityAsync(nameof(FinishReindex), null);
+            var categoryIds =
+                await context.CallActivityAsync<IReadOnlyCollection<string>>(nameof(ReadCategoriesToRebuild), null);
+
+            var rebuildTasks = categoryIds.Select(id =>
+            {
+                var proxy = this.GetCategoryEntityProxy(context, id);
+                return proxy.Rebuild();
+            });
+
+            await Task.WhenAll(rebuildTasks);
+
+            var deleteStateTasks = categoryIds.Select(id =>
+            {
+                var proxy = this.GetCategoryEntityProxy(context, id);
+                return proxy.Delete();
+            });
+
+            await Task.WhenAll(deleteStateTasks);
+
+            await context.CallActivityAsync(nameof(FinishReindex), null);
+        }
+
+        private ICategoryEntity GetCategoryEntityProxy(IDurableOrchestrationContext context, string id)
+        {
+            return context.CreateEntityProxy<ICategoryEntity>(id);
         }
     }
 }
